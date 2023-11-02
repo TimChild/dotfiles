a5896566ea3e2c1a08c26d98e0ed5f83ce327eb1
Tim Child <timjchild@gmail.com>
2023-11-02 09:35:28 -0700

    gitignore added

--- home-ubuntu-scripts/daily-backup.sh
+++ home-ubuntu-scripts/daily-backup.sh
@@ -3,8 +3,18 @@
 # Where to backup to
 BACKUP_DIR="/mnt/g/11Backups/wsl2_ubuntu/daily/"
 
-# What to backup
-PROJECTS_DIR="$HOME/projects"
+declare -A ITEMS_TO_BACKUP
+ITEMS_TO_BACKUP[PROJECTS_DIR]="$HOME/projects"
+ITEMS_TO_BACKUP[CONFIG_DIR]="$HOME/.config"
+ITEMS_TO_BACKUP[SPECIFIC_FILE]="$HOME/path_to_specific_file.txt"
+# Add more directories or files as needed
 
-# Backup projects directory
-rsync -av --delete $PROJECTS_DIR/ $BACKUP_DIR/projects/
+for ITEM in "${!ITEMS_TO_BACKUP[@]}"; do
+    if [ -d "${ITEMS_TO_BACKUP[$ITEM]}" ]; then
+        # Item is a directory
+        rsync -av --delete "${ITEMS_TO_BACKUP[$ITEM]}/" "$BACKUP_DIR/$ITEM/"
+    else
+        # Item is a file
+        rsync -av "${ITEMS_TO_BACKUP[$ITEM]}" "$BACKUP_DIR/$ITEM"
+    fi
+done
