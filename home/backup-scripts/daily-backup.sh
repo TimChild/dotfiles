ba983a7a4c0b4a406ed7b2428c019d33209aaa71
Tim Child <timjchild@gmail.com>
2023-11-02 13:24:13 -0700

    2023-11-02 - automatic daily backup

--- home/backup-scripts/daily-backup.sh
+++ home/backup-scripts/daily-backup.sh
@@ -28,13 +28,15 @@ cd $DOTFILES_DIR
 # Check for changes and commit if any exist (ssh must be set up so user/pass not required)
 if [[ $(git status -s) ]]; then
     git add .
-    #git commit -m "$(date '+%Y-%m-%d') - automatic daily backup"
-    #git push origin main
+    git commit -m "$(date '+%Y-%m-%d') - automatic daily backup"
+    git push origin main
 
     if [ $? -eq 0 ]; then
         echo "Pushed changes to dotfile repo successfully" >> $LOGFILE
     else
         echo "Error encountered while pushing changes." >> $LOGFILE
     fi
+else
+  echo "No changes to push" >> $LOGFILE
 fi
 
