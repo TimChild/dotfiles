57ebeba2444ea1203724cc925a1c75e9d2e8ec21
Tim Child <timjchild@gmail.com>
2024-11-06 17:19:00 -0800

    add commented out bindings

--- configs/nvim_custom/keymaps.lua
+++ configs/nvim_custom/keymaps.lua
@@ -1,16 +1,19 @@
 -- Copy to system clipboard
 vim.keymap.set("n", "<leader>y", '"+y')
 
--- -- Easier commenting (Note: / is _ in keybinding)
--- -- Normal mode
-vim.keymap.set("n", "<C-_>", "gcc", { remap = true, silent = true, desc = "Toggle comment on current line" })
+-- 2024-11-06 -- want to add this back in, but something wrong with setup that is causing errors on commenting
+-- and dont want this confusing things further
 --
--- -- Visual mode
-vim.keymap.set("x", "<C-_>", "gc", { remap = true, silent = true, desc = "Toggle comment on selected lines" })
 --
--- -- Insert mode
-vim.keymap.set("i", "<C-_>", function()
-	-- Exit insert mode and comment the current line (returning cursor to roughly where it was)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>mxgcc`xi", true, false, true), "i", false)
-end, { remap = true, silent = true, desc = "Toggle comment on current line from insert mode" })
---
+-- -- -- Easier commenting (Note: / is _ in keybinding)
+-- -- -- Normal mode
+-- vim.keymap.set("n", "<C-_>", "gcc", { remap = true, silent = true, desc = "Toggle comment on current line" })
+-- --
+-- -- -- Visual mode
+-- vim.keymap.set("x", "<C-_>", "gc", { remap = true, silent = true, desc = "Toggle comment on selected lines" })
+-- --
+-- -- -- Insert mode
+-- vim.keymap.set("i", "<C-_>", function()
+-- 	-- Exit insert mode and comment the current line (returning cursor to roughly where it was)
+-- 	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>mxgcc`xi", true, false, true), "i", false)
+-- end, { remap = true, silent = true, desc = "Toggle comment on current line from insert mode" })
