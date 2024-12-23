a7c2e6b45b3674f3d04fdc6e92f4003b3aae4c82
Tim Child <timjchild@gmail.com>
2024-12-23 12:59:03 -0800

    copy nvim config in init

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -9,9 +9,11 @@ sudo apt-get install xsel
 
 # Setup home directory configs
 ln -s ~/dotfiles/home/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/home/.ssh/config ~/.ssh/config
 
 # Setup configs (that live in config directory)
 ln -s ~/dotfiles/config/tmux ~/.config
+ln -s ~/dotfiles/config/nvim ~/.config
 
 
 
