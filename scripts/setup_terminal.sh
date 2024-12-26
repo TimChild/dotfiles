bfa58fe0b00c25b8bdc475c899f7e693e83c7e8e
Tim Child <timjchild@gmail.com>
2024-12-26 07:48:20 -0800

    setup zsh and ohmyzsh

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -3,17 +3,36 @@
 # This script is to set up the terminal with configurations etc. saved in dotfiles.
 
 
+# # Setup home directory configs
+# ln -s ~/dotfiles/home/.gitconfig ~/.gitconfig
+# ln -s ~/dotfiles/home/.ssh/config ~/.ssh/config
+# ln -s ~/dotfiles/home/.zshrc ~/.zshrc
+# 
+#
+# # Setup configs (that live in config directory)
+# ln -s ~/dotfiles/config/tmux ~/.config
+# ln -s ~/dotfiles/config/nvim ~/.config
+# ln -s ~/dotfiles/config/oh-my-zsh ~/.config
+
+# Install zsh (and oh-my-zsh -- asks to set as default)
+sudo apt install zsh
+ln -s ~/dotfiles/home/.zshrc ~/.zshrc
+sh -c "$(wget -qO- https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended --keep-zshrc
+rm -r ~/.oh-my-zsh/custom
+ln -s ~/dotfiles/config/oh-my-zsh-custom ~/.oh-my-zsh/custom
+
+
 # For clipboard support in tmux (could also use xclip)
 sudo apt-get update
 sudo apt-get install xsel
+# C-compilers (for some nvim things)
+sudo apt-get install gcc
+# Other tools that are useful
+sudo apt-get install ripgrep make
+# For taskfile (classic confinement required)
+sudo snap install task --classic
 
-# Setup home directory configs
-ln -s ~/dotfiles/home/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/home/.ssh/config ~/.ssh/config
 
-# Setup configs (that live in config directory)
-ln -s ~/dotfiles/config/tmux ~/.config
-ln -s ~/dotfiles/config/nvim ~/.config
 
 
 
