ce37adbbbd12605b5a55aa65ee4afab374084fd8
Tim Child <timjchild@gmail.com>
2025-01-31 10:56:00 -0800

    add lazygit

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -144,6 +144,13 @@ source ~/dotfiles/scripts/setup_go.sh
 # Install lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz lazygit
+
 # Install github desktop
 # https://github.com/shiftkey/desktop#installation-via-package-manager
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
