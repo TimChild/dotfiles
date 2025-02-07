a0c35b430edfaf939793a54f2def175fa400c532
Tim Child <timjchild@gmail.com>
2025-02-06 17:13:54 -0800

    add a default setup for reveal presentation

---
+++ webhosting/revealjs_template/content/slides.md
@@ -0,0 +1,18 @@
+## Example first slide title
+
+Enter "S"peaker mode to see notes.
+
+Note: This only shows up in "S"peaker mode.
+notes: Same as this (N/note(s):)
+
+---
+
+## Example second slide title
+
+===
+
+### Vertical slide under second slide
+
+---
+
+## Example third slide title
--- webhosting/scripts/new-revealjs.sh
+++ webhosting/scripts/new-revealjs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Copies the latest version of reveal.js to a specified directory (and installs deps)
 # First argument is the destination directory, second argument is the subdirectory (optional)
-# Note: excludes .git and .github directories
+# Note: excludes .git, .github, and demo.html from the copy
+#
 
-# Check if destination directory is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <dest_dir>"
+# Check arguments passed
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <template_dir> <dest_dir> [sub_dir]"
   exit 1
 fi
 
-
-DEST_DIR="$1"
-SUB_DIR="$2"
+TEMPLATE_DIR="$1"
+DEST_DIR="$2"
+SUB_DIR="$3"
 TEMP_DIR="/tmp/revealjs_clone"
 
 # If the subdirectory is provided, append it to the destination directory
@@ -31,7 +32,9 @@ else
 fi
 
 # Copy contents to destination directory, excluding .git and .github
-rsync -aq --exclude=".git" --exclude=".github" "$TEMP_DIR/" "$DEST_DIR/"
+rsync -aq \
+  --exclude=".git" --exclude=".github" --exclude="demo.html" --exclude="examples" --exclude="test"\
+  "$TEMP_DIR/" "$DEST_DIR/"
 
 echo "Reveal.js copied to $DEST_DIR"
 
@@ -39,7 +42,11 @@ echo "Reveal.js copied to $DEST_DIR"
 npm audit fix --prefix "$DEST_DIR"
 npm install --prefix "$DEST_DIR"
 
-echo "Edit index.html to change the slideshow. Start it by running:"
+# Copy the template files to the destination directory
+rsync -aq "$TEMPLATE_DIR/" "$DEST_DIR/"
+
+echo "Edit 'content/slides.md' to change the slideshow (or modify 'index.html'). Start it by running:"
 echo "npm start --prefix $DEST_DIR"
+echo "Note: Examples can be found in $TEMP_DIR/examples"
 
 
