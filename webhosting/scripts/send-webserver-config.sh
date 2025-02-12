0a3d862760953fb73ff99acb2b31d3a49cb9b940
Tim Child <timjchild@gmail.com>
2025-02-11 16:11:06 -0800

    add webserver/caddy setup

---
+++ webhosting/scripts/send-webserver-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Sends the configuration files required by the webserver (does NOT send all the files that 
+# may need to be served by the webserver)
+
+function heading() {
+    echo "-------"
+    printf "\033[1;32m %s \033[0m \n" "$1"
+    echo "-------"
+}
+
+set -e
+
+droplet_name=$1
+
+if [ -z "$droplet_name" ]; then
+    echo "Usage: send-webserver-config.sh <droplet_name>"
+    exit 1
+fi
+
+heading "Sending webserver config to droplet"
+
+scp {caddy-compose.yaml,Caddyfile} $droplet_name:~/
+
+# Delete and Re-populate the `sites-enabled` directory 
+rsync -av --delete ./sites-enabled/ $droplet_name:~/sites-enabled/
+
+
---
+++ webhosting/scripts/setup_webserver.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Sets up the droplet to act as a web server
+# Sets up Caddy to run as the web server (will restart automatically on system reboot)
+# Caddy will serve static files in `/srv/www` as well as reverse proxy to any services running in other docker containers
+
+
+function heading() {
+    echo "-------"
+    printf "\033[1;32m %s \033[0m \n" "$1"
+    echo "-------"
+}
+
+set -e
+
+heading "Setting up droplet as a webserver..."
+
+# Check caddy-compose.yaml exists
+if [ ! -f caddy-compose.yaml ]; then
+    echo "caddy-compose.yaml not found"
+    exit 1
+fi
+
+# Check Caddyfile exists
+if [ ! -f Caddyfile ]; then
+    echo "Caddyfile not found"
+    exit 1
+fi
+
+# Check sites-enabled directory exists
+if [ ! -d sites-enabled ]; then
+    echo "sites-enabled directory not found"
+    exit 1
+fi
+
+
+# Create a www directory within `/srv`
+sudo mkdir -p /srv/www
+
+# Start Caddy using docker-compose
+docker compose -f caddy-compose.yaml up -d
+
+heading "Webserver setup complete (Caddy is running)"
+
