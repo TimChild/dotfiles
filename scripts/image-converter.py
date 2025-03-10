37903091377fc212114f9eecfaffdc95f792bde5
Tim Child <timjchild@gmail.com>
2025-03-10 15:05:16 -0700

    update image converter script

--- scripts/image-converter.py
+++ scripts/image-converter.py
@@ -13,10 +13,10 @@ import typer
 app = typer.Typer()
 
 ORIG_DIR = "originals"
-SKIP_EXTENSIONS = {".webp", ".ico", ".svg"}
+SKIP_EXTENSIONS = {".ico", ".svg"}
 
 
-def convert_images_to_webp(directory: str = "."):
+def convert_images_to_webp(directory: str, max_width: int, max_height: int) -> None:
     # Create the 'originals' directory if it doesn't exist
     directory_path = Path(directory)
     originals_dir = directory_path / ORIG_DIR
@@ -35,23 +35,32 @@ def convert_images_to_webp(directory: str = "."):
         try:
             # Open an image file
             with Image.open(filepath) as img:
+                # Copy the original file to 'originals' directory
+                original_filename = originals_dir / filepath.name
+                filepath.rename(original_filename)
+
+                # Resize the image
+                img.thumbnail((max_width, max_height))
+
                 # Define the new file name
                 new_filename = filepath.with_suffix(".webp")
 
                 # Convert and save the image as webp
-                img.save(new_filename, "webp")
+                img.save(new_filename, "webp", optimize=True, quality=70, method=6)
 
-                # Move the original file to 'originals' directory
-                filepath.rename(originals_dir / filepath.name)
-                print(f"Converted and moved: {filepath.name}")
+                print(f"Copied and converted: {filepath.name}")
 
         except Exception as e:
             print(f"Skipping {filepath.name}, error: {e}")
 
 
 @app.command()
-def main(directory: str = typer.Argument(".", help="Directory to process images")):
-    convert_images_to_webp(directory)
+def main(
+    directory: str = typer.Argument(".", help="Directory to process images"),
+    max_width: int = typer.Option(1920, help="Max width of the converted images"),
+    max_height: int = typer.Option(1080, help="Max height of the converted images"),
+):
+    convert_images_to_webp(directory, max_width, max_height)
 
 
 if __name__ == "__main__":
