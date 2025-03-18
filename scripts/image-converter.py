ea95c31a5835110cce7e9672617c9654bef9aae3
Tim Child <timjchild@gmail.com>
2025-03-18 16:14:59 -0700

    add quality option to the image-converter

--- scripts/image-converter.py
+++ scripts/image-converter.py
@@ -16,7 +16,9 @@ ORIG_DIR = "originals"
 SKIP_EXTENSIONS = {".ico", ".svg"}
 
 
-def convert_images_to_webp(directory: str, max_width: int, max_height: int) -> None:
+def convert_images_to_webp(
+    directory: str, max_width: int, max_height: int, quality: int
+) -> None:
     # Create the 'originals' directory if it doesn't exist
     directory_path = Path(directory)
     originals_dir = directory_path / ORIG_DIR
@@ -46,7 +48,7 @@ def convert_images_to_webp(directory: str, max_width: int, max_height: int) -> N
                 new_filename = filepath.with_suffix(".webp")
 
                 # Convert and save the image as webp
-                img.save(new_filename, "webp", optimize=True, quality=70, method=6)
+                img.save(new_filename, "webp", optimize=True, quality=quality, method=6)
 
                 print(f"Copied and converted: {filepath.name}")
 
@@ -59,8 +61,9 @@ def main(
     directory: str = typer.Argument(".", help="Directory to process images"),
     max_width: int = typer.Option(1920, help="Max width of the converted images"),
     max_height: int = typer.Option(1080, help="Max height of the converted images"),
+    quality: int = typer.Option(80, help="Quality of the converted images"),
 ):
-    convert_images_to_webp(directory, max_width, max_height)
+    convert_images_to_webp(directory, max_width, max_height, quality)
 
 
 if __name__ == "__main__":
