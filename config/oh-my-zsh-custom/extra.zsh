3e409d3c66f413f03517d2b3b896024839edfdaa
Tim Child <timjchild@gmail.com>
2025-05-11 13:38:17 -0700

    tidy oh-my-zsh config

--- config/oh-my-zsh-custom/extra.zsh
+++ config/oh-my-zsh-custom/extra.zsh
@@ -1,6 +1,10 @@
-source ~/dotfiles/task-completions.zsh
+# Aliases and functions (from aliases.zsh)
+alias vim="nvim"
+alias vi="nvim"
 
-# export BUN_INSTALL="$HOME/.local/share/reflex/bun"
+alias add_alias="vim ~/.oh-my-zsh/custom/extra.zsh"
+alias cd_nvim="cd ~/.config/nvim/lua/custom/"
+
 export PYENV_ROOT="$HOME/.pyenv"
 
 # List of directories to check
@@ -8,7 +12,6 @@ directories=(
     "$HOME/.local/bin"
     "/snap/bin"
     "/opt/nvim/"
-    # "$BUN_INSTALL"
     "$PYENV_ROOT/bin"
 )
 
@@ -24,35 +27,13 @@ done
 # Init pyenv
 eval "$(pyenv init -)"
 
-# # Daily backup
-# if [[ -f ~/dotfiles/.last_daily_backup ]]; then
-#   LAST_MODIFIED=$(date -d "$(cat ~/dotfiles/.last_daily_backup)" +%s)
-#   ONE_DAY=$((24 * 60 * 60))  # 24 hours in seconds
-#
-#   if [[ $(($CURRENT_TIME - $LAST_MODIFIED)) -gt $ONE_DAY ]]; then
-#       echo "INFO: Detected last daily backup older than one day, running ~/backup-scripts/daily-backup.sh now"
-#       # Run your daily backup script
-#       ~/backup-scripts/daily-backup.sh
-#   fi
-# else
-#   echo "WARNING: ~/dotfiles/.last_daily_backup does not exist. Make sure you set up scheduled backups with crontab. See instructions in ~/backup-scripts/readme.md"
-# fi
-# # Weekly backup
-# if [[ -f ~/dotfiles/.last_weekly_backup ]]; then
-#   LAST_MODIFIED=$(date -d "$(cat ~/dotfiles/.last_weekly_backup)" +%s)
-#   ONE_WEEK=$((24 * 60 * 60 * 7))  # 7 days in seconds
-#
-#   if [[ $(($CURRENT_TIME - $LAST_MODIFIED)) -gt $ONE_WEEK ]]; then
-#       echo "INFO: Detected last weekly backup older than one week, running ~/backup-scripts/weekly-backup.sh now"
-#       # Run your daily backup script
-#       ~/backup-scripts/weekly-backup.sh
-#   fi
-# else
-#   if [[ -f ~/dotfiles/.last_daily_backup ]]; then
-#     echo "WARNING: No previous weekly backup, but daily backup found. Running ~/backup-scripts/weekly-backup.sh now"
-#     ~/backup-scripts/weekly-backup.sh
-#   else
-#     echo "WARNING: No weekly or daily backup found. Make sure you set up scheduled backups with crontab. See instructions in ~/backup-scripts/readme.md"
-#   fi
-# fi
+# Completions for gh copilot
+eval "$(gh copilot alias -- zsh)"
 
+# Completions for task
+eval "$(task completion --shell zsh)"
+
+# Cursor appimage
+function cursor {
+        ~/appimages/Cursor-0.49.6-x86_64.AppImage --no-sandbox $@
+}
