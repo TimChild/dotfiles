cf358b933ce09ae262c7ce508b8b8d4288462187
Tim Child <timjchild@gmail.com>
2025-05-11 09:47:23 -0700

    add cursor command

--- config/oh-my-zsh-custom/aliases.zsh
+++ config/oh-my-zsh-custom/aliases.zsh
@@ -4,3 +4,7 @@ alias vi="nvim"
 alias add_alias="vim ~/.oh-my-zsh/custom/aliases.zsh"
 alias cd_nvim="cd ~/.config/nvim/lua/custom/"
 
+function cursor {
+        ~/appimages/Cursor-0.49.6-x86_64.AppImage --no-sandbox $@
+}
+
