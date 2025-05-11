dd37c6c8df57b7496bce25c4aa560855483b1d8e
Tim Child <timjchild@gmail.com>
2025-05-11 09:55:19 -0700

    remove old config and scripts

--- home/backup-scripts/db-backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME="db"
-DB_USER="your_db_user"
-DB_PASSWORD="your_db_password"
-DB_NAME="your_db_name"
-BACKUP_DIR="/path/to/db/backup/directory"
-
-docker exec $CONTAINER_NAME /usr/bin/mysqldump -u $DB_USER --password=$DB_PASSWORD $DB_NAME > $BACKUP_DIR/backup_$(date +"%Y%m%d").sql
